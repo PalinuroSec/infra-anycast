977a004d643526617de6bef1bc77cfece700d7ef
Lorenzo Palinuro Faletra <palinuro@parrotsec.org>
2022-02-10 07:28:05 +0100

    initial commit

---
+++ config/launchbird.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+zerotierip=$(cat /root/zerotier_dump.txt | grep assignedAddresses -A1 | grep 172 | sed -e "s/   \"//" -e "s/\/15\",//")
+zerotierip6=$(cat /root/zerotier_dump.txt | grep assignedAddresses -A2 | grep fd00 | sed -e "s/   \"//" -e "s/\/64\"//"
+fd00:dead:c0de:cafe::402)
+
+cat /etc/bird/bird-template.conf |
+sed -e "s/ZEROTIERIP/$zerotierip/g" |
+sed -e "s/NEIGHBOURIP/$gw4/g" |
+sed -e "s/ASNUM/$as/g" |
+sed -e "s/NEIGHBOURAS/$neighbouras/g" |
+sed -e "s/BGPPASSWORD/$bgppassword/g" > /etc/bird/bird.conf
+
+cat /etc/bird/bird6-template.conf |
+sed -e "s/ZEROTIERIP/$zerotierip/g" |
+sed -e "s/ZEROTIERIP6/$zerotierip6/g" |
+sed -e "s/NEIGHBOURIP6/$gw4/g" |
+sed -e "s/ASNUM/$as/g" |
+sed -e "s/NEIGHBOURAS/$neighbouras/g" |
+sed -e "s/BGPPASSWORD/$bgppassword/g" > /etc/bird/bird6.conf
+
+bird6 -c /etc/bird/bird6.conf
+bird -f -c /etc/bird/bird.conf
---
+++ config/start.sh
@@ -0,0 +1,36 @@
+ip4=${ip4}
+ip6=${ip6}
+gw4=${gw4}
+gw6=${gw6}
+
+dn="dummy0"
+
+sleep .5
+
+modprobe dummy
+
+ip link set $dn down &> /dev/null
+ip link del dev $dn type dummy &> /dev/null
+
+ip link add $dn type dummy
+ip link set $dn up
+
+if [ "" == "`cat /etc/iproute2/rt_tables | grep anycast`" ]; then
+ echo '666       anycast' >> /etc/iproute2/rt_tables
+fi
+
+/etc/init.d/zerotier-one start
+
+for ip in ${ip4[@]}; do
+ ip -4 addr add dev $dn $ip
+ ip -4 rule add from $ip table anycast
+done
+
+ip -4 route add default via $gw4 table anycast
+
+for ip in ${ip6[@]}; do
+ ip -6 addr add dev $dn $ip
+ ip -6 rule add from $ip table anycast
+done
+
+ip -6 route add default via $gw6 table anycast
